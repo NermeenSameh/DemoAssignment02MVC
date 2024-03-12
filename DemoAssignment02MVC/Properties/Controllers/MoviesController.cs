169efe74b39bc305bd043f3256fbe6ed4ee2dcec
NermeenSameh <nermeensameh1998@gmail.com>
2024-03-12 17:43:10 +0200

    Action ReturnTypes

--- DemoAssignment02MVC/Properties/Controllers/MoviesController.cs
+++ DemoAssignment02MVC/Properties/Controllers/MoviesController.cs
@@ -1,18 +1,71 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DemoAssignment02MVC.Properties.Controllers
 {
     public class MoviesController : Controller
     {
-        // Action : Public Non-Static Object Member Function
-        public string GetMovie(int id)
+        private readonly IConfiguration _configuration;
+
+        public MoviesController(IConfiguration configuration)
         {
-            return $"Movie With Id: {id}";
+            _configuration = configuration;
         }
 
-        public string Index()
+        // Action : Public Non-Static Object Member Function
+
+        // [AcceptVerbs("GET" , "POST")]
+        // [HttpPost]
+        public IActionResult GetMovie(int id)
         {
-            return "All Movies";
+            if (id < 10)
+                return new BadRequestResult();
+            if (id == 100)
+                return new NotFoundResult();
+
+            ///   ContentResult result = new ContentResult();
+            ///   result.Content = $"Movie With Id: {id}";
+            ///   result.ContentType = "text/html";
+            ///   result.StatusCode = 404;
+            ///   return result;
+            ///  
+
+            //  return Content($"Movie With Id: {id}");
+            return Content($"Movie With Id: {id}", "text/html");
+
+        }
+
+
+
+        /// public ContentResult GetMovie(int id)
+        /// {
+        ///     ContentResult result = new ContentResult();
+        ///     result.Content = $"Movie With Id: {id}";
+        ///     result.ContentType = "text/html";
+        ///     // result.ContentType = "object/pdf";
+        ///     result.StatusCode = 404;
+        ///     return result;
+        /// }
+
+
+        /// [ActionName("Index")]
+        /// public string GetAllMovies()
+        /// {
+        ///     return "All Movies";
+        /// }
+
+        public IActionResult Hamda()
+        {
+            RedirectResult redirectResult = new RedirectResult($"{_configuration["BaseUrl"]}/Movies/GetMovie/11");
+
+            RedirectToActionResult redirectToActionResult = new RedirectToActionResult(nameof(MoviesController.GetMovie), nameof(MoviesController), new { id = 11 });
+
+            RedirectToRouteResult redirectToRouteResult = new RedirectToRouteResult("Default", new { controller = nameof(MoviesController), action = nameof(GetMovie), id = 11 });
+
+            RedirectToRouteResult redirectToRouteResult01 = new RedirectToRouteResult("Default");
+
+
+            return redirectToRouteResult;
         }
 
     }
